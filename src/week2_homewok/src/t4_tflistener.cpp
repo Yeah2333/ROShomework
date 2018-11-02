c9e222116d79a1ff1c741433087a9c5294c2d602
wangzhiyong <653091941@qq.com>
2018-11-02 20:31:10 +0800

    add t4tflistener annotation

--- src/week2_homewok/src/t4_tflistener.cpp
+++ src/week2_homewok/src/t4_tflistener.cpp
@@ -5,18 +5,19 @@ double a,b,c;
 
 void laserpointCallback(const week2_homework::laserpointPtr &msg)
 {
-    a = msg->x;
+    a = msg->x;//回调函数，存储laser的点
     b = msg->y;
     c = msg->z;
 }
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "tflistener");
-    ros::NodeHandle node;
-    ros::Publisher pub = node.advertise<week2_homework::laserpoint>("/base_link/point",10);
+    ros::NodeHandle node;//ros节点初始化
+
+    ros::Publisher pub = node.advertise<week2_homework::laserpoint>("/base_link/point",10);//创建发布和订阅
     ros::Subscriber sub = node.subscribe("/base_laser/point",10,&laserpointCallback);
 
-    tf::TransformListener listener;
+    tf::TransformListener listener;//tf
 
     ros::Rate rate(10.0);
 
@@ -35,13 +36,13 @@ int main(int argc, char *argv[])
             ros::Duration(1.0).sleep();
             continue;
         }
-        ros::spinOnce();
+        ros::spinOnce();//调用循环函数一次获得laser的点
         week2_homework::laserpoint point;
         point.x = a + transform.getOrigin().x();
         point.y = b + transform.getOrigin().y();
-        point.z = c + transform.getOrigin().z();
-        ROS_INFO("base_laser: %f %f %f -------> base_link: %f %f %f ",a,b,c,point.x,point.y,point.z);
-        pub.publish(point);
+        point.z = c + transform.getOrigin().z();//得到转换坐标后的点
+        ROS_INFO("base_laser: %f %f %f -------> base_link: %f %f %f ",a,b,c,point.x,point.y,point.z);//终端输出
+        pub.publish(point);//发布新的点
 
         rate.sleep();
 
